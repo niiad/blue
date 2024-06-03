74b9db06c3c95d837a735ca8018f89c9cf1649ab
niiad <samuelbrownai@gmail.com>
2024-06-03 11:50:21 +0000

    added models for the user service

---
+++ blueUserService/app/models.py
@@ -0,0 +1,28 @@
+from flask_sqlalchemy import SQLAlchemy
+
+database = SQLAlchemy()
+
+
+class User(database.Model):
+    __tablename__ = "users"
+    id = database.Column(database.Integer, primary_key=True, autoincrement=True)
+    first_name = database.Column(database.String(25), nullable=False)
+    last_name = database.Column(database.String(25), nullable=False)
+    username = database.Column(database.String(30), unique=True, nullable=False)
+    email = database.Column(database.String(120), unique=True, nullable=False)
+    password = database.Column(database.String(128), nullable=False)
+    profile = database.relationship("Profile", uselist=False, back_populates="user")
+
+
+class Profile(database.Model):
+    __tablename__ = "profiles"
+    id = database.Column(database.Integer, primary_key=True, autoincrement=True)
+    user_id = database.Column(database.Integer, database.ForeignKey("users.id"), nullable=False)
+    gender = database.Column(database.String(15))
+    eu_size = database.Column(database.Integer)
+    post_address = database.Column(database.String(15))
+    city = database.Column(database.String(20))
+    region = database.Column(database.String(20))
+    country = database.Column(database.String(25))
+    phone = database.Column(database.String(20))
+    user = database.relationship("User", back_populates="profile")
