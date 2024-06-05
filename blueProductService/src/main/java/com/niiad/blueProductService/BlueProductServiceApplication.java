593ce2a99dbac7fea1cd2da1fbf9c12cbe6caff0
niiad <samuelbrownai@gmail.com>
2024-06-05 20:03:51 +0000

    product microservice set up, open api integration, other dependencies

---
+++ blueProductService/src/main/java/com/niiad/blueProductService/BlueProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.niiad.blueProductService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueProductServiceApplication.class, args);
+	}
+
+}
---
+++ blueProductService/src/test/java/com/niiad/blueProductService/BlueProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niiad.blueProductService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlueProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
